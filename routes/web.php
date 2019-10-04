3eb6563412d19b4d92e7429f0d630f6ea51558eb
Eduardo Jair Cano <jair.ci@incfile.com>
2019-10-03 23:26:54 -0500

    [Routes] Parametros en las rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta basica
+Route::get('saludos', function() {
+   return "Saludos desde la app";
+});
+
+// Ruta con parametros obligatorios
+Route::get('saludos/{nombre}', function($nombre) {
+    return "Hola " . $nombre;
+});
+
+// Ruta con parametros opcionales
+Route::get('despedida/{nombre?}', function($nombre = "usuario") {
+    return "Hasta luego " . $nombre;
+});
