e2f325ed635f9ef358ccc596b4aa4578023aac13
Eduardo Jair Cano <jair.ci@incfile.com>
2019-10-06 17:00:05 -0500

    [Routes] Restructuracion de rutas

--- routes/web.php
+++ routes/web.php
@@ -11,58 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-   //return view('welcome');
-    echo "<a href='" . route('contactos') . "'>Contactos 1</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 2</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 3</a><br>";
-});
-
-// Ruta basica
-Route::get('saludos', function() {
-   return "Saludos desde la app";
-});
-
-// Ruta con parametros obligatorios
-Route::get('saludos/{nombre}', function($nombre) {
-    return "Hola " . $nombre;
-});
-
-// Ruta con parametros opcionales
-Route::get('despedida/{nombre?}', function($nombre = "usuario") {
-    return "Hasta luego " . $nombre;
-});
-
-// Names route
-Route::get('listaContactos', function () {
-    return "Seccion de contactos";
-})->name('contactos');
-
-// Route regresando una View
-Route::get('laravel', function(){
-    return view('welcome');
-})->name('laravel');
-
-// Route regresando una View con parametros por la función view()
-/*Route::get('info', function(){
-    $proyecto = 'Basics Laravel';
-
-    return view('info', ['proyecto' => $proyecto]);
-});*/
-
-// Route regresando una View con parametros por la función view() y compact()
-Route::get('info', function(){
-    $proyecto = 'Basics Laravel';
-
-    return view('info', compact('proyecto'));
-})->name('info');
-
-// Route regresando una View con parametros con metodo ->with()
-/*Route::get('home', function(){
-    $nombre = 'Jair';
-
-    return view('home')->with('nombre', $nombre);
-});*/
-
-// Route con metodo view y parámetros
 Route::view('home', 'home', ['nombre' => 'Jair'])->name('home');
+
+Route::view('info', 'info', ['proyecto' => 'Basics Laravel'])->name('info');
+
+Route::view('laravel', 'welcome')->name('laravel');
